bd9fbd2371f40328c22495a96be47730519b94a5
ssudler <ssudler21@gmail.com>
2021-05-03 17:22:59 -0400

    Initial commit

---
+++ traffic_light.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# Green LED
+GPIO.setup(21, GPIO.OUT)
+# Yellow LED
+GPIO.setup(20, GPIO.OUT)
+# Red LED :)
+GPIO.setup(16, GPIO.OUT)
+
+# Button :/
+GPIO.setup(2, GPIO.IN)
+
+print('Program running')
+
+while True:
+    if not GPIO.input(2):
+        GPIO.output(16, GPIO.HIGH)
+        time.sleep(5)
+        GPIO.output(20, GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(21, GPIO.HIGH)
+        GPIO.output(20, GPIO.LOW)
+        GPIO.output(16, GPIO.LOW)
+        time.sleep(5)
+        GPIO.output(21, GPIO.LOW)
+        GPIO.output(20, GPIO.HIGH)
+        time.sleep(5)
+        GPIO.output(20, GPIO.LOW)
+    else:
+        GPIO.output(16, GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(16, GPIO.LOW)
+        time.sleep(0.2)
+
+GPIO.cleanup()
